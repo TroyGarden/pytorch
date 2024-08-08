558fd5a955cdb7a2adedcdfaae19aa16738c0a2d
Animesh Jain <anijain@umich.edu>
2024-08-08 12:12:46 -0700

    [wip][dynamo] Support object creation of classes with custom __new__
    ghstack-source-id: 323129bd95580454cb0f42fca476ff20901ed57f
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/132977

--- torch/_dynamo/variables/misc.py
+++ torch/_dynamo/variables/misc.py
@@ -204,6 +204,15 @@ class SuperVariable(VariableTracker):
                 self.objvar, attr, variables.DeletedVariable()
             )
             return variables.ConstantVariable(None)
+        elif self.objvar.source and inner_fn is object.__new__:
+            return tx.output.side_effects.track_object_new(
+                self.objvar.source,
+                self.objvar.value,
+                variables.UnspecializedNNModuleVariable
+                if issubclass(self.objvar.value, torch.nn.Module)
+                else UserDefinedObjectVariable,
+                {},
+            )
 
         unimplemented(f"non-function or method super: {inner_fn}")
 
